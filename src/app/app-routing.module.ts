b3e794ff08f97d32b8a6226c49d155acadc4bbe2
Prasheel <prasheelnandwana3@gmail.com>
2020-09-17 22:49:31 +0530

    structure ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component:HomeComponent  },
+  { path:'about', component:AboutComponent  },
+  { path:'work', component:WorkComponent  },
+  { path:'contact', component:ContactComponent  },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { WhoamiComponent } from './about/whoami/whoami.component';
+import { ExpertiseComponent } from './about/expertise/expertise.component';
+import { TimelineComponent } from './about/timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    WorkComponent,
+    ContactComponent,
+    WhoamiComponent,
+    ExpertiseComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
