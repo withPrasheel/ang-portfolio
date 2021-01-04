a5899967aac333db25ed29a4cc5220f21d5e8ff1
Prasheel <prasheelnandwana3@gmail.com>
2021-01-04 11:41:44 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,21 @@ import { WorkComponent } from './work/work.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path:'home', component:HomeComponent  },
-  { path:'about', component:AboutComponent  },
-  { path:'work', component:WorkComponent  },
-  { path:'contact', component:ContactComponent  },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled', // Add options right here
+  })
+  ],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,4 +12,32 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  uliner(key) {
+    // console.log('jchjhdj')
+    document.getElementById('home').classList.remove('uliner');
+    document.getElementById('about').classList.remove('uliner');
+    document.getElementById('work').classList.remove('uliner');
+    document.getElementById('contact').classList.remove('uliner');
+
+    // console.log(key);
+    document.getElementById(key).classList.add('uliner');
+    // switch (key) {
+    //   case 'home':
+    //     document.getElementById('home').classList.add('ulinear');
+    //     break;
+
+    //   case 'about':
+    //     document.getElementById('about').classList.add('ulinear');
+    //     break;
+
+    //   case 'work':
+    //     document.getElementById('work').classList.add('ulinear');
+    //     break;
+
+    //   case 'contact':
+    //     document.getElementById('contact').classList.add('ulinear');
+    //     break;
+    // }
+  }
+
 }
